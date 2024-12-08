acba6600023f9e560cb0b9d758949ab422c027d4
Isaac Spackman <isaacjspackman@gmail.com>
2024-12-08 14:26:26 -0700

    add more pseudocode to describe functionality

--- src/zbinfigs/collection.py
+++ src/zbinfigs/collection.py
@@ -14,13 +14,29 @@ License: MIT
 
 class Collection():
     def __init__():
+        # read a .csv file into a pandas dataframe
+        return
+    
+    def _check_record_file():
+        # check if there is a .pdf or html file
+        return
+    
+    def _convert_html_to_pdf():
+        # clean and convert html files to pdfs
         return
     
     def export_pdfs_to_folder():
+        # for each record, check the record file
+        # if there is a pdf to copy, copy it
+        # if there is an html file to process, convert to pdf and copy it
         return
     
     def plot_pdfs_available():
+        # make a plot that shows the number of records that were extractable
+        # relative to the total number of records
         return
 
     def plot_annotations():
+        # make a plot of the distribution of annotations based on the records
+        # that could be processed
         return
--- src/zbinfigs/utils.py
+++ src/zbinfigs/utils.py
@@ -12,19 +12,32 @@ License: MIT
 """
 
 def read_collection():
+    # create a collection object from a .csv file
     return
 
 def process_pdf_folder():
+    # process the selected pdf files into markdown
+    # first read all the .pdf files in the current directory that 
+    # match the expected format
+    # then only process selected
     return
 
 def gather_figures():
+    # locate the image files for the .pdfs in the range selected
+    # and merge into a single .pdf 
+    # export the page ranges for each record to a .csv
     return
 
 def merge_summary_pdfs():
+    # merge pdfs into a single document and make an updated
+    # page range metadata file
     return
 
 def sort_annotated():
+    # locate the images based on their annotations and sort into 
+    # folders accordingly - rename the figures to avoid name clashes
     return
 
 def add_annotations():
+    # add annotations to the main .csv file
     return
